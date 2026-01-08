effa3f24c743b178abdf97cda8b92270d7ec9cd4
Alexander Khristichenko <alexander.khristichenko@gmail.com>
2026-01-09 00:27:55 +0200

    added main screen and logic

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,26 +7,36 @@ public class GameManager : MonoBehaviour
     public static GameManager instance;
 
     int score = 0;
+    bool gameStarted = false;
 
     [SerializeField] GameObject target;
     [SerializeField] Transform spawnPoint;
     [SerializeField] float spawnRate;
     [SerializeField] float maxPos;
     [SerializeField] TextMeshProUGUI scoreText;
+    [SerializeField] GameObject startScreen;
 
     void Awake()
     {
         instance = this;
     }
 
-    void Start()
+    void Update()
     {
-        GameStart();
+        if (Input.GetMouseButtonDown(0) && !gameStarted)
+        {
+            GameStart();
+
+            gameStarted = true;
+        }
     }
 
     public void GameStart()
     {
         InvokeRepeating("SpawnTarget", 1f, spawnRate);
+
+        startScreen.SetActive(false);
+        scoreText.gameObject.SetActive(true);
     }
 
     void SpawnTarget()
@@ -41,7 +51,6 @@ public class GameManager : MonoBehaviour
     {
         score++;
         scoreText.text = score.ToString();
-        print(score);
     }
 
     public void GameOver()
